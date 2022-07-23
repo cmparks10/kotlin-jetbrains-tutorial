2f36ea1b8848ab001369bbb74247f2a4badc9112
Corey Parks <cmparks10@gmail.com>
2022-07-23 18:36:22 -0400

    Adding a small method

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,4 +4,12 @@ fun main(args: Array<String>) {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+    var sumNum = sum(4, 5);
+    println(sumNum);
+    val firstName = "Corey"
+
+}
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
 }
